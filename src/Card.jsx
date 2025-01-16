59f7b60e37b2dd3df2964c83cc0a8a4ff2cf914f
karlo0505 <106652921+karlo0505@users.noreply.github.com>
2025-01-16 20:55:16 +0800

    My first commit

---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+  return (
+    <div>
+      <img src="" alt="Profile Picture"></img>
+      <h2>Karl</h2>
+      <p>I'm learning react to create a website.</p>
+    </div>
+  );
+}
+export default Card;
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} My First React Website</p>
+    </footer>
+  );
+}
+export default Footer;
